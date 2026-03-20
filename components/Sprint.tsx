2713cb50fb83f92b2717eaf77512e05e1c8ca8f3
ilyaskuratovsky <ilyaskuratovsky@gmail.com>
2026-03-20 08:57:39 -0400

    project switcher

--- components/Sprint.tsx
+++ components/Sprint.tsx
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, StyleSheet } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import Timer from "./Timer";
 import CurrentBank from "./CurrentBank";
 import { useDatabaseContext } from "../database/DatabaseContext";
+import ProjectSwitcherHeader from "./ProjectSwitcherHeader";
 
 const Sprint: React.FC = () => {
   const insets = useSafeAreaInsets();
   const db = useDatabaseContext();
+  const projects = [
+    {
+      id: "1",
+      name: "Work",
+    },
+    {
+      id: "2",
+      name: "Side Project",
+    },
+  ];
+
+  const [activeIndex, setActiveIndex] = useState(0);
 
   return (
     <View
@@ -18,6 +31,13 @@ const Sprint: React.FC = () => {
         justifyContent: "center",
       }}
     >
+      <ProjectSwitcherHeader
+        projects={projects}
+        activeIndex={activeIndex}
+        onChange={(index) => {
+          setActiveIndex(index);
+        }}
+      />
       <View style={styles.timerContainer}>
         <Timer
           bankTime={async (seconds: number) => {
